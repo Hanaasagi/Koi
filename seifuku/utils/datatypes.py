9e50a0bd2ddc9569e77e24652295304acf95e2e4
Hanaasagi <ambiguous404@gmail.com>
2017-10-08 20:19:34 +0900

    add from_dict

--- seifuku/utils/datatypes.py
+++ seifuku/utils/datatypes.py
@@ -2,7 +2,7 @@ from collections import UserDict
 from seifuku.utils.helpers import is_dunder
 
 # for mypy
-from typing import Any
+from typing import Any, Hashable
 
 
 class Config(UserDict):
@@ -15,6 +15,14 @@ class Config(UserDict):
     True
     >>> config.BAR == 2
     True
+    >>> nested_dict = {
+    ...     'mysql': {
+    ...         'driver': 'pymysql'
+    ...     }
+    ... }
+    >>> config = Config.from_dict(nested_dict)
+    >>> config.mysql.driver
+    'pymysql'
     """
     @classmethod
     def from_object(cls, obj: Any):
@@ -23,6 +31,16 @@ class Config(UserDict):
         for attr in dir(obj):
             if (not is_dunder(attr)) and attr.isupper():
                 config.data[attr] = getattr(obj, attr)
+        return config
+
+    @classmethod
+    def from_dict(cls, data_dict: dict):
+        config = cls()
+        for key, value in data_dict.items():
+            if isinstance(value, dict):
+                config[key] = cls.from_dict(value)
+            else:
+                config[key] = value
         return config
 
     def __getattr__(self, attr: str) -> Any:
